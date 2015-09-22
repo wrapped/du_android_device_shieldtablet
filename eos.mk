fcfc39f49dbe60259290a996c173f9f57325b6ff
wrapped <ddroidx@gmail.com>
2015-09-22 09:49:38 -0500

    Shieldtablet: Update bootanimation
    Change-Id: I7a241453cd612bcb276167c8f8ef23c8005d37ab

--- eos.mk
+++ eos.mk
@@ -1,6 +1,10 @@
 # Inherit device configuration for shieldtablet.
 $(call inherit-product, device/nvidia/shieldtablet/full_shieldtablet.mk)
 
+# Copy Bootanimation
+PRODUCT_COPY_FILES += \
+vendor/eos/prebuilt/common/bootanimation/bootanimation.zip:system/media/bootanimation.zip
+
 # Inherit some common eos stuff.
 $(call inherit-product, vendor/eos/config/common_full_phone.mk)
 
