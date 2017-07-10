02b1c6364de46a624438708fe6f878100ffba137
Wrapped <ddroidx@gmail.com>
2017-07-10 00:28:21 -0500

    shieldtablet: Make it Dirty

--- du.mk
+++ du.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, device/nvidia/shieldtablet/full_shieldtablet.mk)
 
 # Inherit some common lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_shieldtablet
+PRODUCT_NAME := du_shieldtablet
 PRODUCT_DEVICE := shieldtablet
